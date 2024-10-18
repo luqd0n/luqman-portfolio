e1d6b0656d1dc748949b2d3d525a070ab192e184
luqd0n <luqmanhakimjohar@gmail.com>
2024-10-19 02:15:09 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Scroll smoothly to sections when nav links are clicked
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
